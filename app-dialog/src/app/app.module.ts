226f267b2926f6dc76cd3d3e1cdbad7fc28e7c39
Alex Korzhikov <korzio@gmail.com>
2022-02-13 20:41:36 +0100

    update app-dialog to include routing for single-spa
    https://single-spa.js.org/docs/ecosystem-angular/
    including zone.js https://single-spa.js.org/docs/ecosystem-angular/#configure-routes
    
    add fix for webpack single-spa
    https://github.com/single-spa/single-spa-angular/issues/395

--- app-dialog/src/app/app.module.ts
+++ app-dialog/src/app/app.module.ts
@@ -2,21 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDialogModule} from '@angular/material/dialog';
+import {APP_BASE_HREF} from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: '**', component: EmptyRouteComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatIconModule,
     MatDialogModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
